27da5c52afe41af7ed960404ae60f9f6a1164d6e
shreya-1604 <155795212+shreya-1604@users.noreply.github.com>
2025-02-03 16:08:28 +0530

    Add files via upload

---
+++ add_workshop.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+def add(a, b):
+    return a + b
+
+# Example usage
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+result = add(num1, num2)
+print(f"The result of addition is: {result}")
+
