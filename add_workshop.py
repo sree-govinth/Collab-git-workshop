ef3a7aa6b7ce5978db97a07d3e3607e73929e96f
sree-govinth <155771157+sree-govinth@users.noreply.github.com>
2025-02-03 16:34:24 +0530

    Update add_workshop.py

--- add_workshop.py
+++ add_workshop.py
@@ -8,9 +8,8 @@ def add(a, b):
     return a + b
 
 # Example usage
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
+num1 = 20
+num2 = 10
 
 result = add(num1, num2)
-print(f"The result of addition is: {result}")
 
